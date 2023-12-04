026fadf2e59c4443209ec02baa5e4ce7218fdd03
Joe Previte <jjprevite@gmail.com>
2023-12-04 15:43:56 +0000

    refactor: use accent color in h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline  text-secondary">
+        Hello world!
+      </h1>
     </div>
   )
 }
