07c623a6259543fbf334d8651b4d24d1f0a31943
Joe Previte <jjprevite@gmail.com>
2023-12-06 03:50:58 +0000

    feat: add <InitialScreen />

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { InitialScreen } from "./components/InitialScreen"
+
 function App() {
   return (
     <div className="prose">
-      <h1 className="text-3xl font-bold underline  text-secondary">
-        Hello world!
-      </h1>
+      <InitialScreen />
     </div>
   )
 }
