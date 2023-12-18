097824b5c0d8e5d0b90d05c45e8f7ec8ff3c2b56
Joe Previte <jjprevite@gmail.com>
2023-12-18 03:06:11 +0000

    Refactor DoneScreen component and add copy to clipboard functionality

--- src/components/DoneScreen/DoneScreen.tsx
+++ src/components/DoneScreen/DoneScreen.tsx
@@ -31,10 +31,23 @@ export function DoneScreen({
               ))}
             </ul>
           </div>
-          <div className="glass mx-2 md:pb-4 px-4">
+          <div className="glass mx-2 pb-4 px-4">
             <h1 className="pt-4 mb-2">words ignored</h1>
             <div className="divider w-5/6 mx-auto"></div>
             <p className="italic">{ignoreWordsAsString}</p>
+            <button
+              className="btn btn-primary btn-md font-bold mx-auto block"
+              onClick={async () => {
+                try {
+                  await navigator.clipboard.writeText(ignoreWordsAsString)
+                  console.log("Copying to clipboard was successful!")
+                } catch (err) {
+                  console.error("Could not copy text: ", err)
+                }
+              }}
+            >
+              Copy to clipboard
+            </button>
           </div>
         </div>
       </div>
