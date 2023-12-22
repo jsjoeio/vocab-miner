a7355f7fc6d4387c43e2926b1e7e1e00f1dd737f
Joe Previte <jjprevite@gmail.com>
2023-12-22 04:40:57 +0000

    fix: highlight word updates

--- src/components/DoneScreen/MinedWord.tsx
+++ src/components/DoneScreen/MinedWord.tsx
@@ -1,15 +1,10 @@
+import { highlightWord } from "../../models/VocabularyMiner"
+
 interface MinedWordProps {
   word: string
   sentence: string | null
 }
-const highlightWord = (sentence: string, word: string) => {
-  // Replace this with your actual logic to highlight the word in the sentence
-  const regex = new RegExp(`\\b${word}\\b`, "gi")
-  return sentence.replace(
-    regex,
-    (match) => `<span class="font-medium">${match}</span>`
-  )
-}
+
 
 export function MinedWord({ word, sentence }: MinedWordProps) {
   const sentenceWithWordBolded = sentence ? highlightWord(sentence, word) : ""
