46a12db5e3db6a04aca619b4e0e5f786337f8da3
Dan Kim <guynameddan@gmail.com>
2023-12-25 15:43:16 -0700

    Click to copy (#45)
    Feature update for #23
    
    ---------
    
    Co-authored-by: Joe Previte <jjprevite@gmail.com>

--- src/components/DoneScreen/MinedWord.tsx
+++ src/components/DoneScreen/MinedWord.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { highlightWord } from "../../models/VocabularyMiner"
 
 interface MinedWordProps {
@@ -8,10 +9,45 @@ interface MinedWordProps {
 
 export function MinedWord({ word, sentence }: MinedWordProps) {
   const sentenceWithWordBolded = sentence ? highlightWord(sentence, word) : ""
+  const [isCopied, setIsCopied] = React.useState(false)
+
+  React.useEffect(() => {
+    if (isCopied) {
+      const timeout1 = setTimeout(() => {
+        document.getElementsByClassName("minedWord")[0].classList.remove("tooltip", "tooltip-right")
+      }, 1000)
+
+      const timeout2 = setTimeout(() => {
+        document.getElementsByClassName("minedWord")[0].classList.add("tooltip", "tooltip-right")
+        setIsCopied(false)
+      }, 2000)
+      return () => {
+        clearTimeout(timeout1)
+        clearTimeout(timeout2)
+      }
+    }
+  }, [isCopied])
 
   return (
     <li className="mb-4">
-      <span className="font-semibold text-lg">{word}</span>
+      <div
+        className="tooltip tooltip-right minedWord"
+        data-tip={isCopied ? "Copied!" : "Copy to clipboard"}
+      >
+        <button
+          className="font-semibold text-lg"
+          onClick={async () => {
+            try {
+              await navigator.clipboard.writeText(word)
+              setIsCopied(true)
+            } catch (err) {
+              console.log("Could not copy text: ", err)
+            }
+          }}
+        >
+          {word}
+        </button>
+      </div>
       <span
         className="block italic font-light"
         dangerouslySetInnerHTML={{ __html: sentenceWithWordBolded }}
